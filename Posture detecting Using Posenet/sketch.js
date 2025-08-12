ef15d12de6dc2a348a49dd9d8c0539818da71572
SayorHasan <md.sayor.hasan@g.bracu.ac.bd>
2025-08-12 14:35:37 +0600

    begins

---
+++ Posture detecting Using Posenet/sketch.js
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>
+            PosenetDemo
+        </title>\
+        <script src='sketch.js'></script>
+    </head>
+    <body>
+        <h1>Posenet Demo</h1>
+    </body>
+</html>
