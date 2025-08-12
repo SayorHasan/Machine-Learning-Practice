fa6b80b0025b81f8f91bd435e89bbf4a0efde4bb
SayorHasan <md.sayor.hasan@g.bracu.ac.bd>
2025-08-12 15:38:45 +0600

    pose detect

--- Posture detecting Using Posenet/sketch.js
+++ Posture detecting Using Posenet/sketch.js
@@ -1,1 +1,62 @@
-alert('hello world')
+let video;
+let bodyPose;
+let poses = [];
+let connections;
+
+function preload() {
+  
+  bodyPose = ml5.bodyPose();
+}
+
+function setup() {
+  createCanvas(640, 480);
+
+  
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+
+  bodyPose.detectStart(video, gotPoses);
+  
+  connections = bodyPose.getSkeleton();
+}
+
+function draw() {
+  
+  image(video, 0, 0, width, height);
+
+  
+  for (let i = 0; i < poses.length; i++) {
+    let pose = poses[i];
+    for (let j = 0; j < connections.length; j++) {
+      let pointAIndex = connections[j][0];
+      let pointBIndex = connections[j][1];
+      let pointA = pose.keypoints[pointAIndex];
+      let pointB = pose.keypoints[pointBIndex];
+      if (pointA.confidence > 0.1 && pointB.confidence > 0.1) {
+        stroke(255, 0, 0);
+        strokeWeight(2);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+      }
+    }
+  }
+
+  
+  for (let i = 0; i < poses.length; i++) {
+    let pose = poses[i];
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      let keypoint = pose.keypoints[j];
+      if (keypoint.confidence > 0.1) {
+        fill(0, 255, 0);
+        noStroke();
+        circle(keypoint.x, keypoint.y, 10);
+      }
+    }
+  }
+}
+
+
+function gotPoses(results) {
+  
+  poses = results;
+}
